1ff2a3ceb679f4a327a75a70769befef55cb3ff2
GodwinJoe06 <godwinjoe2005@gmail.com>
2025-07-25 08:34:29 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   role: { type: String, enum: ['user', 'admin'], default: 'user'},
-  batch: { type: String, enum: ['Batch 1', 'Batch 2', 'Batch 3']},
+  batch: { type: String, enum: ['Batch 1', 'Batch 2']},
 });
 
 module.exports = mongoose.model('User', userSchema);
